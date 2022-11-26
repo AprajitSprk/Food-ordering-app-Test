15a12daac611d8dc0bba9140d81e0bbfeb47db2f
Paddy1102 <prasad.khanvilkar321@gmail.com>
2022-11-26 14:14:30 +0530

    map changes

--- client/src/Components/MapHotels/MapHotels.js
+++ client/src/Components/MapHotels/MapHotels.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function MapHotels() {
   return (
-    <div>
-        <h2>hello</h2>
+    <div className='map_hotel'>
+        <div>
+        <div class="mapouter">
+            <div class="gmap_canvas">
+                <iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d4664.09567422162!2d73.05696484930661!3d19.02599382304227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sRestaurants!5e0!3m2!1sen!2sin!4v1669451812114!5m2!1sen!2sin"></iframe>
+                <a href="https://formatjson.org/"></a></div></div>
+        </div>
+        
     </div>
   )
 }
